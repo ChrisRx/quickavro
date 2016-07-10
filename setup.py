385bf9cd464feecb5c5fbe7b7b5fb95d060275f5
ChrisRx <flynt@oldtech.co>
2016-07-10 10:56:28 -0400

    [WIP] Fixing build and adding appveyor

--- setup.py
+++ setup.py
@@ -8,60 +8,147 @@ import glob
 
 import pip
 from setuptools import setup, Extension, find_packages
+from distutils.ccompiler import new_compiler
 
-from setup_utils import *
 
+WIN = sys.platform.startswith('win')
 PY2 = sys.version_info[0] == 2
 PY3 = sys.version_info[0] == 3
 
 if not PY3:
     FileNotFoundError = (IOError, OSError)
 
+STATIC_BUILD_DIR = "build/static"
+STATIC_LIB_NAME = "quickavro"
+if WIN:
+    STATIC_LIB = "{0}/{1}.lib".format(STATIC_BUILD_DIR, STATIC_LIB_NAME)
+else:
+    STATIC_LIB = "{0}/lib{1}.a".format(STATIC_BUILD_DIR, STATIC_LIB_NAME)
+
+
+def exists(path):
+    if os.path.isdir(path):
+        return True
+    try:
+        with open(path):
+            return True
+    except (OSError, IOError):
+        return False
+
+def get_version():
+    version_regex = re.compile(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', re.MULTILINE)
+    with open('quickavro/__init__.py', 'r') as f:
+        return version_regex.search(f.read()).group(1)
+
+def is_64bit():
+    if sys.maxsize > 2**32:
+        return True
+    return False
+
+
+class StaticCompiler(object):
+    name = None
+    version = None
+    url = None
+
+    include_dirs = []
+    source_dir = None
+    excluded = []
+    sources = []
+    depends = []
+    libraries = []
+
+    extra_compile_args = []
+
+    def __init__(self):
+        self.c = new_compiler()
+        self.include_dirs.append(os.path.join(os.getcwd(), "src"))
+        if WIN:
+            self.default_compile_args = [
+                '-D_WIN32',
+                '-D_CRT_SECURE_NO_WARNINGS',
+                '-DWIN32',
+                '-DNDEBUG',
+                '-D_WINDOWS',
+                '-D__x86_64__',
+                '-D__i386__',
+            ]
+        else:
+            self.default_compile_args = ['-O3', '-fPIC', '-g', '-Wall', '-Wfatal-errors']
+        if not exists(STATIC_BUILD_DIR):
+            os.makedirs(STATIC_BUILD_DIR)
+        self.setup()
+
+    def compile(self, force=False):
+        if exists(os.path.join(STATIC_BUILD_DIR, self.static_lib_name)) and not force:
+            return
+        sys.stderr.write("Compiling {0} to static library ...\n".format(self.name))
+        sys.stderr.write("="*32)
+        sys.stderr.write("\n")
+        self.sources = []
+        for ext in {'c', 'cc'}:
+            self.sources.extend(glob.glob("{0}/*.{1}".format(self.source_dir, ext)))
+        self.depends = []
+        for ext in {'h', 'hpp'}:
+            self.depends.extend(glob.glob("{0}/*.{1}".format(self.source_dir, ext)))
+        for exclude in self.excluded:
+            matches = [s for s in self.sources if exclude in s]
+            if matches:
+                for match in matches:
+                    self.sources.remove(match)
+        objs = self.c.compile(self.sources,
+            include_dirs=self.include_dirs,
+            extra_preargs=self.default_compile_args + self.extra_compile_args,
+            depends=self.depends
+        )
+        if self.libraries:
+            for lib in self.libraries:
+                o = glob.glob("{0}/*.o".format(lib.source_dir))
+                objs.extend(o)
+
+        self.c.create_static_lib(objs, self.name.lower(), output_dir=STATIC_BUILD_DIR)
+        sys.stderr.write("\n")
+        return objs
+
+    def setup(self):
+        pass
+
+    @property
+    def static_lib_name(self):
+        if WIN:
+            return "{0}.lib".format(self.name.lower())
+        else:
+            return "lib{0}.a".format(self.name.lower())
+
+    @property
+    def static_library(self):
+        return os.path.join(STATIC_BUILD_DIR, self.static_lib_name)
+
+
 class Jansson(StaticCompiler):
     name = "Jansson"
     version = "2.7"
-    url = "https://github.com/akheron/jansson/archive/v{0}.tar.gz".format(version)
-    filename = "jansson-{0}.tar.gz".format(version)
-
     include_dirs = [
-        'vendor/jansson-{0}'.format(version),
-        'vendor/jansson-{0}/src'.format(version),
+        "vendor/jansson/src",
     ]
-    source_dir = "vendor/jansson-{0}/src".format(version)
+    source_dir = "vendor/jansson/src"
     extra_compile_args = ['-DHAVE_STDINT_H', '-DJSON_INLINE=inline']
-
-    def setup(self):
-        try:
-            touch(os.path.join(self.source_dir, "jansson_config.h"))
-        except FileNotFoundError as error:
-            pass
-        if WIN and PY2:
-            filename = "{0}/utf.h".format(self.source_dir)
-            diff = [(16, "#include <stdint.h>\n", "#include <pstdint.h>\n")]
-            patch(filename, diff)
 
 
 class Snappy(StaticCompiler):
     name = "Snappy"
     if WIN:
         version = "1.1.1.8"
-        url = "https://github.com/kiddlu/snappy-windows/archive/master.tar.gz"
-        filename = "snappy-windows-{0}.tar.gz".format(version)
-
         include_dirs = [
-            'vendor/snappy-windows-master/src',
+            "vendor/snappy-windows/src",
         ]
-        source_dir = "vendor/snappy-windows-master/src"
+        source_dir = "vendor/snappy-windows/src"
     else:
         version = "1.1.3"
-        url = "https://github.com/google/snappy/releases/download/{0}/snappy-{0}.tar.gz".format(version)
-        filename = "snappy-{0}.tar.gz".format(version)
-
         include_dirs = [
-            'vendor/snappy-{0}'.format(version),
+            "vendor/snappy/src"
         ]
-        source_dir = "vendor/snappy-{0}".format(version)
-
+        source_dir = "vendor/snappy/src"
     excluded = [
         "snappy-test.cc",
         "snappy_unittest.cc",
@@ -72,37 +159,22 @@ class Snappy(StaticCompiler):
 class AvroC(StaticCompiler):
     name = "Avro"
     version = "1.8.0"
-    url = "https://github.com/apache/avro/archive/release-{0}.tar.gz".format(version)
-    filename = "avro-{0}.tar.gz".format(version)
-
     include_dirs = [
-        'vendor/avro-release-{0}/lang/c/src'.format(version),
-        'vendor/avro-release-{0}/lang/c/src/avro'.format(version)
+        "vendor/avro/src",
+        "vendor/avro/src/avro",
     ]
+    if not WIN:
+        excluded = ["generic.cc"]
     include_dirs.extend(Jansson.include_dirs)
     libraries = [Jansson]
-    source_dir = "vendor/avro-release-{0}/lang/c/src".format(version)
-    excluded = [
-        "schema_specific.c",
-        "avroappend.c",
-        "avrocat.c",
-        "avromod.c",
-        "avropipe.c",
-    ]
+    source_dir = "vendor/avro/src"
     extra_compile_args = ['-DHAVE_STDINT_H', '-DJSON_INLINE=inline']
-
-    def setup(self):
-        filename = "{0}/avro_private.h".format(self.source_dir)
-        diff = [(37, "#define snprintf _snprintf\n", "#if _MSC_VER < 1900\n#define snprintf _snprintf\n#endif\n")]
-        patch(filename, diff)
 
 
 def compile_ext():
     force = True if '--force' in sys.argv else False
     libs = [Snappy(), Jansson(), AvroC()]
     for lib in libs:
-        lib.download()
-        lib.extract()
         lib.compile(force)
     include_dirs = [
         os.path.join(os.getcwd(), "src"),
